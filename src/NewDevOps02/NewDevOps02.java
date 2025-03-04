b6c018bda4d8939dfed84ae8d323474938fd716b
UmaBS-200694 <umabs622@gmail.com>
2025-03-04 15:36:18 +0530

    Addtion of two numbers

---
+++ src/NewDevOps02/NewDevOps02.java
@@ -0,0 +1,19 @@
+package NewDevOps02;
+
+public class NewDevOps02 {
+	static int i1;
+	static int i2;
+
+	public static void main(String[] args) {
+  i1=20;
+  i2=30;
+  addTion();
+
+	}
+	public static void addTion() {
+		
+		int sum=i1+i2;
+		System.out.println("Sum of two numbers: " + sum );
+	}
+
+}
