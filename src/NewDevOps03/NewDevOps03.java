2234c19c5f8617b06e9559f47e76a59a62a7ef17
UmaBS-200694 <umabs622@gmail.com>
2025-03-04 19:30:45 +0530

    First Assignment

---
+++ src/NewDevOps03/NewDevOps03.java
@@ -0,0 +1,18 @@
+package NewDevOps03;
+
+public class NewDevOps03 {
+	
+
+	public static void main(String[] args) {
+ M1();
+	}
+	public static void M1() {
+		
+	
+		System.out.println("First Assignment " );
+	}
+
+
+
+
+}
